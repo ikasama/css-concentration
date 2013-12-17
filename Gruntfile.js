b9f2b5e8ce8e1a9d9e1c0c5f09285fe8562c2b4e
Masaaki Morishita <ikasama3.0@gmail.com>
2013-12-17 20:04:17 +0900

    add grunt plugin for stylus

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,25 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
+
+    stylus: {
+      compile: {
+        options: {
+        },
+        files: {
+          'css/css-concentration.css': ['styl/*.styl']
+        }
+      }
+    },
+
+    watch: {
+      stylus: {
+        files: ['styl/*.styl'],
+        tasks: ['stylus']
+      }
+    }
 
   });
 
